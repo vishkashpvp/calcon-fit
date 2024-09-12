1d21fd191d9cd3e77f920deac75302349dce4902
vishkashpvp <vishkash.vishnu@gmail.com>
2024-09-12 23:48:05 +0530

    update home page text to 'coming soon'

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center font-[family-name:var(--font-geist-sans)]">
-      <main>
+      <main className="p-3">
         <div className="text-5xl">
+          We are <span className="cursor-help hover:underline">coming</span> s<span>👀</span>n
+        </div>
+        <div className="text-5xl hidden">
           Calc <span className="text-green-500">On</span> <br />
           <span className="italic">Cal Con</span>
         </div>
